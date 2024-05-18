67cf383004ed418309085285abe91cb15f9744cb
Matias Rosales <145402946+Matias473@users.noreply.github.com>
2024-05-18 00:42:10 +0000

    git commit --amend

---
+++ parcial1/2_variables_tipos_datos/variables.py
@@ -0,0 +1,26 @@
+"""las variables son debilmente tipadas
+es decir no es necesario indicar el tipo 
+de dato al momento de utilizrla
+"""
+
+#variables
+edad = 19
+estatura = 1.70
+email="Matias.Rosales.473@gmail.com"
+logica = True
+caracter = "a"
+
+#imprimir el contenido de variables
+print(edad)
+print(estatura)
+print(email)
+print(logica)
+print(caracter)
+
+#conocer el tipo de dato que python asigno
+
+print(type(edad))
+print(type(estatura))
+print(type(email))
+print(type(logica))
+print(type(caracter))
