6235bd827b27334f5346519f965787f2b467b9b5
Matias473 <Matias.Rosales.473@gmail.com>
2024-05-21 17:55:01 -0600

    git commit --amend

---
+++ parcial1/6_ejercicios_repaso/ejercicio4.py
@@ -0,0 +1,26 @@
+#4.-Solicitar 2 numeros al usuario
+# y realizar todas las operaciones
+# basicas de una calculadora (+,-,*,/)
+# y mostrar por pantalla el resultado
+
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+
+print("=== SUMA ===")
+suma = numero1 + numero2
+print(suma)
+print("\n")
+
+print("=== RESTA ===")
+resta = numero1 - numero2
+print(resta)
+print("\n")
+
+print("=== MULTIPLICACION ===")
+mult = numero1 * numero2
+print(mult)
+print("\n")
+
+print("=== DIVISION ===")
+division = numero1 / numero2
+print(division)
