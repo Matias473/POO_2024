620a1f61edd01431ecd7e7dabfc7299811123ac3
Matias473 <Matias.Rosales.473@gmail.com>
2024-05-21 18:06:03 -0600

    si

---
+++ parcial1/6_ejercicios_repaso/ejercicio8.py
@@ -0,0 +1,11 @@
+#8.- Hacer un programa que resuelva lo siguiente. 
+#¿Cuanto es el X por ciento de X numero?
+
+porcentaje = int(input("Ingrese el porcentaje que desea calcular: "))
+numero = int(input("Ingrese el numero al cual desea obtener el porcentaje: "))
+
+porcentaje_red = porcentaje / 100
+
+res = numero * porcentaje_red
+
+print(f"El porcentaje ({porcentaje}%) de {numero} es: {res}")
