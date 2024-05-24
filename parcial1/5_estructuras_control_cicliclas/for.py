c4cb22163337cadfed3498000ef4f701c88c675e
Matias473 <Matias.Rosales.473@gmail.com>
2024-05-24 10:07:01 -0600

    si

--- parcial1/5_estructuras_control_cicliclas/for.py
+++ parcial1/5_estructuras_control_cicliclas/for.py
@@ -11,11 +11,11 @@
 #     print("@")
  
 #Ejemplo 2 Crear un programa que imprima los numeros del 1 a 5, los sume e imprima la suma al final
-# suma=0
-# for contador in range(1,6):
-#     print(contador)
-#     suma+=contador
-# print(f"La suma es: {suma}")
+suma=0
+for contador in range(1,6):
+   print(contador)
+   suma+=contador
+print(f"La suma es: {suma}")
 
 
 # Ejemplo 3 Crear un programa que solicite un numero entero y apartir de este numero genere e imprima la tabla de multiplicar correspondiente
