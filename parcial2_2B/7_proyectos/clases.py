9592ef944333beaf5e3d30dcfda523a2b2a60dd3
Matias473 <Matias.Rosales.473@gmail.com>
2024-06-26 11:12:47 -0600

    ER Parcial2 POO of VSCode

---
+++ parcial2_2B/7_proyectos/clases.py
@@ -0,0 +1,10 @@
+class figuras:
+    #atributos
+    def __init__(self,x,y,visible):
+        self.x = x
+        self.y = y
+        self.visible = visible
+    #metodos
+    def visible(self):
+        return True
+    
